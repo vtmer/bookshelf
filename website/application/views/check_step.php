6ba2cb7e19074467f11cd7fe255e4dd52e5ca7ec
zhangchile <zzlchile@gmail.com>
2013-04-20 17:17:05 +0800

    增加check_step 页面

---
+++ website/application/views/check_step.php
@@ -0,0 +1,82 @@
+
+<div class="main">
+	<div class="mid_content">
+		<h3>借书流程</h3>
+		<p class="step_01"><span>1</span>搜索选择自己要借的书</p>
+		<p class="dots"><span></span></p>
+		<p class="dots"><span></span></p>
+		<p class="dots"><span></span></p>
+		<p class="step_02"><span>2</span>电话或者短信联系</p>
+		<div class="content_box">
+			<div class="box_demo search_results">
+				<h2>书源人联系方式：</h2>
+				<p><span>名称:xxx</span><span>年级：11级</span><span>长号：123456789</span><span>短号：123456</span><span>东区</span></p>
+				<span class="tips">tips:在您联系书源人时请考虑当前时间，选择使用短信或是电话联系</span>
+				<h4>您要借的书籍：</h4>
+				<table>
+					<tbody>
+						<tr>
+							<th>书名</th>
+							<th>课程名称</th>
+							<th>作者</th>
+							<th>发放对象</th>
+							<th>出版社</th>
+							<th>版次</th>
+						</tr>
+						<tr>
+							<td>线性代数</td>
+							<td>线性代数</td>
+							<td>李钊</td>
+							<td>大二</td>
+							<td>人民出版社</td>
+							<td>n次</td>
+						</tr>
+						<tr>
+							<td>大学物理</td>
+							<td>大学物理</td>
+							<td>爱因斯坦</td>
+							<td>大二</td>
+							<td>人民出版社</td>
+							<td>n次</td>
+						</tr>
+						<tr>
+							<td>大学英语</td>
+							<td>大学英语</td>
+							<td>韩梅梅</td>
+							<td>大二</td>
+							<td>人民出版社</td>
+							<td>n次</td>
+						</tr>
+						<tr>
+							<td>高等数学</td>
+							<td>高等数学</td>
+							<td>华罗庚</td>
+							<td>大二</td>
+							<td>人民出版社</td>
+							<td>n次</td>
+						</tr>
+						<tr>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td></td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+			<div class="bottom_shadow"></div><!-- 块级区域下方的底层阴影 -->
+		</div><!-- 包裹整块质感效果的div -->
+		<p class="dots"><span></span></p>
+		<p class="dots"><span></span></p>
+		<p class="step_03"><span>3</span>确定预约结果</p>
+		<div class="receipt">
+			<a href="repeipt.html" class="success"></a>
+			<span>,or</span>
+			<a href="repeipt.html" class="un_success"></a>
+			<span>?</span>
+			<p>tips:若有某一本书预约失败可在回执中取消</p>
+		</div>
+	</div><!--end of mid_content-->
+</div><!--end of main-->
