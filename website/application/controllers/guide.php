274505e4e294434ee29b8fa6105cd103c0d30d93
sayue <likehuyi92@gmail.com>
2013-04-25 19:05:01 +0800

    引导页面完成

--- website/application/controllers/guide.php
+++ website/application/controllers/guide.php
@@ -9,19 +9,26 @@ class Guide extends CI_Controller
 
 	public function index()
 	{
-		$datas = array(
-			'one' => '机电工程学院',
-			'two' => '自动化学院',
-			'three' => '轻工化工学院',
-			'four' => '信息工程学院',
-			'five' => '土木与交通学院',	
-			'six' => '计算机学院',
-			'seven' => '材料与能源学院',
-			'eight' => '环境科学与工程学院',
-			'nine' => '外国语学院',
-			'ten' => '物理与光电工程学院');
+		$this->load->view('guide');
+	}
 
-		$this->load->view('guide',$datas);
+	public function choose()
+	{
+		//$faculty = $this->input->post('faculty');
+		$major = $this->input->post('major');
+		$grade = $this->input->post('grade');
+
+		//echo $faculty."</br>".$major."</br>".$grade;
+		$data = array(
+			'truename' => NULL,	
+			'uid' => NULL,
+			'points' => NULL,
+			'major' => $major,
+			'grade' => $grade,
+			'is_logged_in' => FALSE,
+		);
+		$this->session->set_userdata($data);
+		$this->load->view('home');
 	}
 }
 
