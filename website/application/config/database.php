eb554a3ce9dc3590deeb9f62708df171e09cf480
hbc <bcxxxxxx@gmail.com>
2013-05-04 16:40:48 +0800

    oops

--- website/application/config/database.php
+++ website/application/config/database.php
@@ -49,10 +49,10 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'root';
-$db['default']['password'] = 'likehuyi';
+$db['default']['username'] = 'bookshelf';
+$db['default']['password'] = 'book';
 $db['default']['database'] = 'bookshelf';
-$db['default']['dbdriver'] = 'mysql';
+$db['default']['dbdriver'] = 'mysqli';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
 $db['default']['db_debug'] = TRUE;
