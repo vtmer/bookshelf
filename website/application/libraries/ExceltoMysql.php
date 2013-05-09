eb613a403515fd97580b725a122bb5edfefa8884
zhangchile <zzlchile@gmail.com>
2013-05-09 18:36:20 +0800

    增加了从Excel批量导入到数据库的类。在本地已测试成功。具体要求看文件注释

---
+++ website/application/libraries/ExceltoMysql.php
@@ -0,0 +1,94 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed'); 
+/**
+ *从Excel批量导入数据到MySql
+ *注意Excel必须先转换为CSV格式，首行为字段名且与数据库中的顺序一致，
+ *然后用记事本把编码转换为UTF-8
+ * @access public
+ * @param $table,$file
+ * @author chile
+ * @return 成功导入的行数$n
+ */
+class ExceltoMysql
+{
+	protected $table;
+	protected $file;
+	protected $field;
+	protected $fieldNum;
+	public function __construct()
+	{
+		switch(func_num_args())
+		{
+			case 2:$this->ExceltoMysql2(func_get_arg(0),func_get_arg(1));break;
+			default:$this->ExceltoMysql();break;
+		}
+	}
+	protected function ExceltoMysql() 
+	{
+		echo "this is default ";
+	}
+	protected function ExceltoMysql2($table,$file)
+	{
+		$this->table = $table;
+		$this->file = $file;
+		$this->get_field();
+	}
+	public function set($table,$file)
+	{
+		$this->table = $table;
+		$this->file = $file;
+		$this->get_field();
+	}
+	protected function get_field()
+	{
+		if($this->table == '') exit("Please set tablename!");
+		$query = mysql_query("SHOW COLUMNS FROM $this->table");
+		$this->fieldNum = mysql_num_fields($query);
+		while($fieldArray = mysql_fetch_assoc($query))
+		{
+			$this->field[] = $fieldArray['Field'];
+		}
+	}
+	public function InsertToMysql()
+	{
+	    $handle = fopen($this->file,'r') or exit("Unable to open $this->file file!");
+	    //字段匹配验证
+	    $firstRow = fgetcsv($handle);
+	    if(count($firstRow)!=$this->fieldNum)
+	    {
+	    	exit("Unmatch fields' number!");
+		}
+		if(count($diff = array_diff($firstRow,$this->field))) 
+		{
+			$error = "";
+	    	foreach ($diff as $key => $value) 
+	    	{
+	    		$error .= " '$value' ";
+	    	}
+	    	exit('Please check field:'.$error."in the file $this->file!");
+		}
+		//END
+		$sql = "INSERT INTO $this->table VALUES (";	
+		$n = 2;
+	    while($charArray = fgetcsv($handle))//这里是第二行开始读取了
+	    {
+	    	$sql2 = "";
+	    	foreach ($charArray as $key => $value) 
+	    	{
+	    		if($key!=$this->fieldNum-1)
+	    		{
+	    			$sql2 .= "'".$charArray[$key]."',";
+	    		}
+	    		else
+	    		{
+	    			$sql2 .= "'".$charArray[$key]."');";
+	    		}
+	    	}
+	    	$sql3 = $sql.$sql2;
+	    	mysql_query($sql3) or exit("已成功导入".(int)($n-1)."行，错误发生在".$n."行；<br/>错误信息：".mysql_error());
+	    	$n++;
+	    }
+	    return $n;   	
+	}
+}
+
+/* End of file ExceltoMysql.php */
