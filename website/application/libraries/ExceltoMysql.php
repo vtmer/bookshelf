b0c23e0205e26f1da6a099f1924037e4d678f5fb
zhangchile <zzlchile@gmail.com>
2013-05-25 15:53:53 +0800

    更新ExceltoMysql.php: 1、数据可以同时插入到两个表。 3、excel中只需在最后增加两个字段列就可以了

--- website/application/libraries/ExceltoMysql.php
+++ website/application/libraries/ExceltoMysql.php
@@ -9,15 +9,15 @@
  */
 class ExceltoMysql
 {
-	protected $table;
-	protected $file;
-	protected $field;
-	protected $fieldNum;
+	protected $table = 'allbook';		//表名
+	protected $file;					//文件目录
+	protected $field;					//字段名
+	protected $fieldNum;				//字段数目
 	public function __construct()
 	{
 		switch(func_num_args())
 		{
-			case 2:$this->ExceltoMysql2(func_get_arg(0),func_get_arg(1));break;
+			case 1:$this->ExceltoMysql2(func_get_arg(0));break;
 			default:$this->ExceltoMysql();break;
 		}
 	}
@@ -25,15 +25,13 @@ class ExceltoMysql
 	{
 		
 	}
-	protected function ExceltoMysql2($table,$file)
+	protected function ExceltoMysql2($file)
 	{
-		$this->table = $table;
 		$this->file = $file;
 		$this->get_field();
 	}
-	public function set($table,$file)
+	public function setFilePath($file)//设置文件目录
 	{
-		$this->table = $table;
 		$this->file = $file;
 		$this->get_field();
 	}
@@ -52,8 +50,8 @@ class ExceltoMysql
 		mysql_query("SET NAMES gbk");
 	    $handle = fopen($this->file,'r') or exit("Unable to open $this->file file!");
 	    //字段匹配验证
-	    $firstRow = fgetcsv($handle);
-	    if(count($firstRow)!=$this->fieldNum)
+	    $firstRow = fgetcsv($handle);//读取第一行
+	    if(count($firstRow)!=($this->fieldNum+2))
 	    {
 	    	exit("Unmatch fields' number!");
 		}
@@ -62,32 +60,53 @@ class ExceltoMysql
 			$error = "";
 	    	foreach ($diff as $key => $value) 
 	    	{
-	    		$error .= " '$value' ";
+	    		if($value=='major'||$value=='grade')
+	    		{}
+	    		else
+	    		{
+	    			$error .= " '$value' ";
+	    		}	
 	    	}
-	    	exit('Please check field:'.$error."in the file $this->file!");
+	    	if($error!="")
+	    	{
+	    		exit('Please check field:'.$error."in the file $this->file!");
+	    	}
 		}
 		//END
 		$sql = "INSERT INTO $this->table VALUES (";	
 		$n = 2;
+		$string =  "SELECT auto_increment as id FROM information_schema.`TABLES` WHERE TABLE_NAME='$this->table'";
+		(int)$lastRowID = mysql_fetch_assoc(mysql_query($string));//获取自增的allbook.id的下一个值
+		(int)$i = 0;
 	    while($charArray = fgetcsv($handle))//这里是第二行开始读取了
 	    {
 	    	$sql2 = "";
 	    	foreach ($charArray as $key => $value) 
 	    	{
-	    		if($key!=$this->fieldNum-1)
+	    		if($key<11)
 	    		{
-	    			$sql2 .= "trim(both from '".$charArray[$key]."'),";
+		    		if($key<10)
+		    		{
+		    			$sql2 .= "trim(both from '".$value."'),";
+		    		}
+		    		else
+		    		{
+		    			$sql2 .= "trim(both from '".$value."'));";
+		    		}
 	    		}
-	    		else
+	    		elseif($key==11)
 	    		{
-	    			$sql2 .= "trim(both from '".$charArray[$key]."'));";
-	    		}
+	    			
+	    			mysql_query("INSERT INTO `allbook_mg` VALUES(null,trim(both from '".$charArray[11]."'),trim(both from '".$charArray[12]."'),'".(int)($lastRowID['id']+$i)."')") 
+	    			or exit(mysql_error());
+	    		}	
 	    	}
 	    	$sql3 = $sql.$sql2;
 	    	mysql_query($sql3) or exit("已成功导入".(int)($n-2)."行，错误发生在".$n."行；<br/>错误信息：".mysql_error());
 	    	$n++;
+	    	$i++;
 	    }
-	    return $n;   	
+	    return $n-2;   	
 	}
 }
 
