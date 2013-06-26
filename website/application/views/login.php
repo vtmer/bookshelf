929c2e2e56c37c44ae8abb94438b6e0f81251ebf
seventhZhao <seventh.zhao@gmail.com>
2013-06-26 14:21:37 +0800

    登录页修改
    增加自动登录选择、增加忘记密码链接（不知道是哪里，你们改一下）
    调整登录按钮样式

--- website/application/views/login.php
+++ website/application/views/login.php
@@ -5,6 +5,24 @@
 <title>登录页</title>
 <link href="<?php echo base_url('css/reset.css'); ?>" type="text/css" rel="stylesheet" />
 <link href="<?php echo base_url('css/login.css'); ?>" type="text/css" rel="stylesheet" />
+<style type="text/css">
+/* popboxcss */
+.popbox{width:350px;background-color:rgba(219,213,189,1);padding:1px;position:absolute;top:0;left:0;display:none;z-index:120;  
+    /* 设置阴影 */
+    -webkit-box-shadow:1px 1px 3px #292929;
+    -moz-box-shadow:1px 1px 3px #292929;
+    box-shadow:1px 3px 13px #292929;
+	}
+.popbox h2{height:25px;font-size:14px;background-color:rgba(224,188,138,1);position:relative;padding-left:10px;line-height:25px;color:#fff;}
+.popbox h2 a{position:absolute;right:5px;font-size:12px;color:#fff;}
+.popbox .mainlist{padding:10px;}
+.popbox .mainlist span{margin:5px 5px 5px 5px;font-family:"宋体";font-size:14px;font-weight:400;color:#000;}
+.popbox .mainlist div{margin:5px 5px 30px 5px;}
+.popbox .ok-btn{height:30px; border:none; margin-left:99px; background:#c6c4b6; padding:0 17px; box-shadow:0 -2px 4px -1px rgba(0,0,0,.6); border-radius:5px; font-size:14px; line-height:28px; text-align:center; cursor:pointer;}
+.popbox .ok-btn:hover { background:#e8a126; color:#fff;} 
+#screen{width:100%;height:100%;position:absolute;top:0;left:0;display:none;z-index:100;background-color:#000;opacity:0.5;filter:alpha(opacity=50);-moz-opacity:0.5;}
+/*popboxcss end*/
+</style>
 <script language="javascript" src="<?php echo base_url('js/jquery-1.7.2.js'); ?>"></script>
 <script language="javascript" src="<?php echo base_url('js/pop_box.js'); ?>">
 </script>
@@ -20,7 +38,9 @@
 		<form action="<?php echo site_url('login/check'); ?>" method="POST" class="ajaxForm"> 
 			<span>已有帐号登录:</span>
 			<label for="user_name">用户名：<input type="text" name="username"/ ></label>
-			<label for="password">密码：<input type="password" name="pwd" /></label>	
+			<label for="password">密码：<input type="password" name="pwd" /></label>
+			<label for="remember" class="check_label"><input type="checkbox" id="remember"/><span unselectable="on">自动登录</span></label>
+			<a href="#" class="forget">忘记密码？</a>
 			<input type="submit" class="confirm" value="登陆" />
 		</form>
 		<a href="<?php echo site_url('register');?>" class="sign_up"></a>
@@ -43,24 +63,7 @@
 <script>
 document.getElementById('home_page').getElementsByTagName('a')[0].innerText= " ";
 </script>
-<style type="text/css">
-/* popboxcss */
-.popbox{width:350px;background-color:rgba(219,213,189,1);padding:1px;position:absolute;top:0;left:0;display:none;z-index:120;  
-    /* 设置阴影 */
-    -webkit-box-shadow:1px 1px 3px #292929;
-    -moz-box-shadow:1px 1px 3px #292929;
-    box-shadow:1px 3px 13px #292929;
-	}
-.popbox h2{height:25px;font-size:14px;background-color:rgba(224,188,138,1);position:relative;padding-left:10px;line-height:25px;color:#fff;}
-.popbox h2 a{position:absolute;right:5px;font-size:12px;color:#fff;}
-.popbox .mainlist{padding:10px;}
-.popbox .mainlist span{margin:5px 5px 5px 5px;font-family:"宋体";font-size:14px;font-weight:400;color:#000;}
-.popbox .mainlist div{margin:5px 5px 30px 5px;}
-.popbox .ok-btn{height:30px; border:none; margin-left:99px; background:#c6c4b6; padding:0 17px; box-shadow:0 -2px 4px -1px rgba(0,0,0,.6); border-radius:5px; font-size:14px; line-height:28px; text-align:center; cursor:pointer;}
-.popbox .ok-btn:hover { background:#e8a126; color:#fff;} 
-#screen{width:100%;height:100%;position:absolute;top:0;left:0;display:none;z-index:100;background-color:#000;opacity:0.5;filter:alpha(opacity=50);-moz-opacity:0.5;}
-/*popboxcss end*/
-</style>
+
 
 <div id="screen"></div><!--screen end-->
 
