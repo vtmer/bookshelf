609dcf662294f0b76c852685e3cec70339c8bcd2
seventhZhao <seventh.zhao@gmail.com>
2013-06-27 21:55:49 +0800

    修改首页样式

--- website/js/home.js
+++ website/js/home.js
@@ -3,6 +3,8 @@ $(function(){
 	$(".main .match_book .hidden_list").bind("mouseover",function(){
 		$(".main .match_book .hidden_list").removeClass("hover_td");
 		$(this).addClass("hover_td");
+	}).bind("mouseout",function(){
+		$(this).removeClass("hover_td");
 	});
 	$(".select_all").bind("click", function(){
 		$(this).parent().prev().find("input[type=checkbox]").attr("checked", "checked");
