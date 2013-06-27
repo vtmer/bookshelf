c5d805e115c7a4382607b667e9f737dc9238011a
seventhZhao <seventh.zhao@gmail.com>
2013-06-27 21:57:48 +0800

    删除无用代码

--- website/js/message.js
+++ website/js/message.js
@@ -1,4 +1,3 @@
-$(".main .search_bar input").bind("click",function(){if(this.value=="请输入要查找的书目")this.value=""}).bind("blur",function(){if(!this.value)this.value="请输入要查找的书目"});
 $(".main .message .message_box h5").bind("click",function(){
 	$(this).next().is(':visible')?$(this).removeClass("click_h5").next().hide() : $(this).addClass("click_h5").next().show();
 })
