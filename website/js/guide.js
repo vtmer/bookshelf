1771d9f6028811395a221ac2316dab51ea4e3269
seventhZhao <seventh.zhao@gmail.com>
2013-06-27 14:20:19 +0800

    更新校区选择效果。

--- website/js/guide.js
+++ website/js/guide.js
@@ -39,7 +39,34 @@ var gg_college = {
 	{
 		"college" : "物理与光电工程学院",
 		"major" : ["电子科学与技术（光电子技术、微电子技术方向）","光信息科学与技术"]
-	}]
+	},
+	//以下是龙洞校区的学院信息
+	{
+		"college" : "管理学院",
+		"major" : ["工商管理","市场营销","人力管理资源","物流管理","管理科学","信息管理与信息系统","电子商务","会计学","财务管理","土地资源管理","旅游管理"]
+	},
+	{
+		"college" : "经济与贸易学院",
+		"major" : ["经济学","投资学","国际经济与贸易","会展经济与管理"]
+	},
+	{
+		"college" : "应用数学学院",
+		"major" : ["信息与计算科学（信息计算方向）","信息与计算科学（信息安全方向）","统计学"]
+	},
+	//以下是东风路校区的学院信息
+	{
+		"college" : "建筑学院",
+		"major" : ["建筑学","城市规划","景观设计"]
+	},
+	{
+		"college" : "艺术设计学院",
+		"major" : ["工业设计","服装与服装工程","视觉传达","环境设计","数字媒体与动画","美术"]
+	},
+	{
+		"college" : "政法学院",
+		"major" : ["法学","社会工作","公共事业管理"]
+	}
+	]
 }
 
 function setMajor(get_college){
@@ -60,24 +87,40 @@ function setMajor(get_college){
 	});
 }
 $(function(){
-	$("#home_page a").text(" ");
+	//校区选择的动作
+	$(".campus li").bind("click",function(){
+		if(!$(this).hasClass("current")){
+			$(".campus li").removeClass("current")
+			var x = $(this).addClass("current").detach();$(".campus").prepend(x);
+			$(".first_step ul").hide();
+			var get_college = $(this).attr("data-no");
+			$("." + get_college).show();
+		}
+	});
+
+	// 下面是第一步，选择学院的动作
 	$(".main .first_step ul li span.college").bind("click",function(){
-	$("form input#college_select").val($(this).next().text());
-	setMajor($(this).next().text());
-	$(".selected_logo span").addClass(this.className);	
-	$(".main .first_step").hide();$(".main .selected_logo").show();$(".main .second_step").show();
+		$("form input#college_select").val($(this).next().text());
+		$("form input#campus_select").val($(".campus .current").attr("data-no"));
+		setMajor($(this).next().text());
+		$(".selected_logo span").addClass(this.className).addClass($(".current").attr("data-no"));	
+		$(".main .first_step").hide();$(".main .selected_logo").show();$(".main .second_step").show();
+	});
+	// 第二步动作放在setMajor内绑定。
+	// 下面是返回键的动作
+	$(".main .second_step .step_back").bind("click",function(){
+		$("form input#college_select").val(" ");
+		$(".main .selected_logo").hide();$(".main .second_step").hide();$(".main .first_step").show();
+	});
+	$(".main .third_step .step_back").bind("click",function(){
+		$("form input#major_select").val(" ");
+		$("form input#grade_select").val(" ");
+		$(".main .third_step").hide();$("form #submit").hide();$(".main .second_step").show();
+	});
+
+	// 下面是第三步的动作
+	$(".main .third_step ul li").bind("click",function(){
+		$("form input#grade_select").val($(this).text())  ;
+		$("form").submit();
+	})
 });
-$(".main .second_step .step_back").bind("click",function(){
-	$("form input#college_select").val(" ");
-	$(".main .selected_logo").hide();$(".main .second_step").hide();$(".main .first_step").show();
-});
-$(".main .third_step .step_back").bind("click",function(){
-	$("form input#major_select").val(" ");
-	$("form input#grade_select").val(" ");
-	$(".main .third_step").hide();$("form #submit").hide();$(".main .second_step").show();
-});
-$(".main .third_step ul li").bind("click",function(){
-	$("form input#grade_select").val($(this).text())  ;
-	$("form").submit();
-})
-})
