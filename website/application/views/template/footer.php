2c1291ee633401d4213092d678ac4ee57e5e97bf
seventhZhao <seventh.zhao@gmail.com>
2013-07-01 17:26:32 +0800

    底部链接修正

--- website/application/views/template/footer.php
+++ website/application/views/template/footer.php
@@ -2,7 +2,7 @@
 	<div class="safa"></div>
 	<ul>
 		<li>Copyright © 2013 Product Of vtmer <a href="http://weibo.com/vtmer">维生数工作室</a></li>
-		<li><a href="mailto=:gdutbookshelf@163.com">联系我们</a> gdutbookshelf@163.com <a href="http://www.vtmer.com">关于我们</a> <a href="<?php echo site_url('admin'); ?>">后台管理</a> 工大书架</li>
+		<li><a href="mailto:gdutbookshelf@163.com" title="gdutbookshelf@163.com">联系我们</a> gdutbookshelf@163.com <a href="http://www.vtmer.com">关于我们</a> <a href="<?php echo site_url('admin'); ?>">后台管理</a> 工大书架</li>
 
 	</ul>
 </div>
