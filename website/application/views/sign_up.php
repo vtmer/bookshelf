f6959b6980c5871526e920ea52f83f6d8ca4afb2
zhangchile <zzlchile@gmail.com>
2013-07-11 21:33:06 +0800

    注册页面修复低栏

--- website/application/views/sign_up.php
+++ website/application/views/sign_up.php
@@ -1,44 +1,11 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>加入工大书架</title>
-<link href="<?php echo base_url('css/reset.css'); ?>" type="text/css" rel="stylesheet" />
-<link href="<?php echo base_url('css/header.css'); ?>" type="text/css" rel="stylesheet" />
-<link href="<?php echo base_url('css/sign_up.css'); ?>" type="text/css" rel="stylesheet" />
-<script language="javascript" src="<?php echo base_url('js/jquery-1.7.2.js'); ?>"></script>
-<script language="javascript" src="<?php echo base_url('js/pop_box.js');?>"></script>
-</head>
-<body>
-<div id="float_head">
-	<div class="header">
-		<a href="<?php echo site_url('home');?>" id="gdutonline"></a>
-		<!--<span class="score">积分：0</span>
-		<a href="message.html" id="message">收到短信息<span>(0)</span></a>  
-		<div class="user_info">
-			<span class="user_name">董晓丁</span>
-			<a href="personal_info.html">个人设置</a>
-			<a href="bookshelf.html">我的书架</a>
-			<a href="#">退出</a>
-		</div>
-		<a href="add_book.html" id="add_book">捐书</a>-->
-	</div>
-</div>
-<?php include("./application/views/template/pop_box.php");?>
-<div class="home_page">
-	<a href="<?php echo site_url('home');?>" title="前往首页"></a>
-</div>
+
 <div class="main">
-	<!--<div class="search_bar">
-		<input type="text" class="search_input" placeholder="请输入要查找的书目" />
-		<input type="submit" class="search_submit" value=""/>
-	</div>-->
 	<div class="mid_content">
 		<!--<h3>个人信息设置：</h3>-->
 		<div class="content_box">
 			<div class="box_demo sign_info">
 			<img src="<?php echo base_url('img/join_us.jpg'); ?>" alt="加入我们" class="join_us"/>
-				<form action="<?php echo site_url('register/check'); ?>" method="post" class="ajaxForm">
+				<form action="<?php echo site_url('register/check'); ?>" method="post" class="">
 					<label for="mail"><span>邮箱</span>
 						<input type="text" id="mail" name="username"/>
 						<span class="notice">填写常用邮箱以便验证</span>
