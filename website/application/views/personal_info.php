01532f34f5d1ca5d1effd12d745b675b33b5b0cc
lemonpick <lemonpick5@gmail.com>
2013-07-19 14:55:31 +0800

    个人信息页样式完成，新增的邮箱还未做验证&自动补全

--- website/application/views/personal_info.php
+++ website/application/views/personal_info.php
@@ -9,15 +9,16 @@
 			<div class="box_demo personal_config">
 				<form action="<?php echo site_url('home/personal_config');?>" method="post" class="ajaxForm">
 				<div class="left">
-					<label>姓名：<span class="user_name"><?php echo $user[0]['truename'];?></span></label>
-					<label>账号：<span class="user_name"><?php echo $user[0]['username'];?></span></label>			
-					<input type="button" class="config_submit" value="修改密码" id="config_pwd"/>
+				<div><!--用于边框-->
+					<label>用户名：<span class="user_name"><?php echo $user[0]['truename'];?></span></label>
+		
+<!-- 					<input type="button" class="config_submit" value="修改密码" id="config_pwd"/>
 					<div id="div_pwd" class="div_pwd">
 						<label for="pwd_old">旧密码：<input type="password" name="pwd_old" id="pwd_old"/></label>
 					<label for="password">新密码：<input type="password" name="pwd" id="password" /></label>
 					<label for="password_once">确认密码：<input type="password" name="pwd2" id="password_once"/></label>
 					<input type="button" class="config_submit" value="取消" id="cancle_pwd"/>
-					</div>
+					</div> -->
 					<label for="college">学院：<span class="user_name" ><?php echo $user[0]['faculty'];?></label>
 					<label for="major">专业：<span class="user_name"   ><?php echo $user[0]['major'];?></label>
 					<label for="grade">年级：<span class="user_name"  ><?php echo $user[0]['grade'];?></label>
@@ -37,19 +38,25 @@
                             <span class="user_name"><?php echo $user[0]['dormitory']; ?></span>
 							<?php endif;?>
 						</div>
-					</label> 
-					<input type="submit" class="config_submit" value="好了，修改完了，保存吧！" />
+					</label>
+					<!-- 邮箱的修改&验证还没有 -->
+					<label for="mail">邮箱地址：<input type="text" class="mail" name="mail" value="<?php echo $user[0]['username'];?>"/></label>
+					<input type="submit" class="config_submit" value="保存" />
+				</div>
 				</div>
 				</form>
 				<div class="right">
 					<ul>
-						<li><?php echo $user[0]['truename'];?></li>
-						<li class="gray"><?php echo $user[0]['faculty'];?></li>
-						<li class="gray"><?php echo $user[0]['grade']."级 ".$user[0]['major'];?></li>
+						<li><h5>您的书架记录</h5></li>
+						<!-- <li><?php echo $user[0]['truename'];?></li> -->
+						<!-- <li class="gray"><?php echo $user[0]['faculty'];?></li> -->
+						<!-- <li class="gray"><?php echo $user[0]['grade']."级 ".$user[0]['major'];?></li> -->
 						<li>捐书：<?php echo $book_num[0][0];?>本</li>
 						<li>借入：<?php echo $book_num[1][0];?>本</li>
 						<li>借出：<?php echo $book_num[2][0];?>本</li>
-						<li>分享度：<?php echo $user['share'];?>%</li>
+						<li>积分：<?php echo $this->user_model->show_user_point($this->session->userdata('points')); ?></li>
+						<li><a href="#" title="Q&A">查看积分规则</a></li>
+						<!-- <li>分享度：<?php echo $user['share'];?>%</li> -->
 					</ul>
 				</div>
 				
