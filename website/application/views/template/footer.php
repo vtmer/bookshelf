c58e270e8ffde907021e506d4f9c377cefbbd6fc
lemonpick <lemonpick5@gmail.com>
2013-07-20 14:23:33 +0800

    FOOTER 去掉 vtmer

--- website/application/views/template/footer.php
+++ website/application/views/template/footer.php
@@ -1,7 +1,7 @@
 <div class="footer">
 	<div class="safa"></div>
 	<ul>
-		<li>Copyright © 2013 Product Of vtmer <a href="http://weibo.com/vtmer">维生数工作室</a></li>
+		<li>Copyright © 2013 Product Of <a href="http://weibo.com/vtmer">维生数工作室</a></li>
 		<li><a href="mailto:gdutbookshelf@163.com" title="gdutbookshelf@163.com">联系我们</a> gdutbookshelf@163.com <a href="http://www.vtmer.com">关于我们</a> 工大书架</li>
 
 	</ul>
