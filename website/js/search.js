51bd239cb4de46852a47bb6efdfdfa1f203a4831
zhangchile <zzlchile@gmail.com>
2013-07-25 20:58:49 +0800

    优化搜索 ajax分页显示

--- website/js/search.js
+++ website/js/search.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 $('html,body').animate({scrollTop:500},1000);
 $(".ajax_page").bind('click', function(){
 var url = $(this).attr("href").replace(/search/,"search/ajaxPage");
-        $.get(url,{t:Math.random()},function(data){
+        $.get(url,{},function(data){
         	 if (typeof data !== 'object') {
 	        	    jsonobj = JSON.parse(data);
                         } else {
@@ -17,14 +17,16 @@ function update(dat)
 {
 	var link = document.URL.substr(0,document.URL.indexOf('search'));
 	var	data = dat['books'];
+	//删除所有tr，除了第一行
+	$("table tr:gt(0)").empty();
 	for(var i=0;i<data.length;i++)
 	{
-		$("tr:eq("+(i+1)+") td:nth-child(1)").html("<a href='"+link+"home/book_info/"+data[i].id+"'>"+data[i].name+"</a>");
-		$("tr:eq("+(i+1)+") td:nth-child(2)").html(data[i].course_name);
-		$("tr:eq("+(i+1)+") td:nth-child(3)").html(data[i].author);
-		$("tr:eq("+(i+1)+") td:nth-child(4)").html(data[i].course_category);
-		$("tr:eq("+(i+1)+") td:nth-child(5)").html(data[i].publish);
-		$("tr:eq("+(i+1)+") td:nth-child(6)").html(data[i].version);
+		$("tr:eq("+(i+1)+")").append("<td><a href='"+link+"home/book_info/"+data[i].id+"'>"+data[i].name+"</a></td>");
+		$("tr:eq("+(i+1)+")").append("<td>"+data[i].course_name+"</td>");
+		$("tr:eq("+(i+1)+")").append("<td>"+data[i].author+"</td>");
+		$("tr:eq("+(i+1)+")").append("<td>"+data[i].course_category+"</td>");
+		$("tr:eq("+(i+1)+")").append("<td>"+data[i].publish+"</td>");
+		$("tr:eq("+(i+1)+")").append("<td>"+data[i].version+"</td>");
 	}
 	//替换分页
 	var	page = dat['page'];
@@ -32,7 +34,7 @@ function update(dat)
 	//重新绑定事件
 	$(".ajax_page").bind('click', function(){
 	var url = $(this).attr("href").replace(/search/,"search/ajaxPage");
-	        $.get(url,{t:Math.random()},function(data){
+	        $.get(url,{},function(data){
 	        	if (typeof data !== 'object') {
 	        	    jsonobj = JSON.parse(data);
                         } else {
