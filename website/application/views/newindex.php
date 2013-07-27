fdf093b50db1137bc7e21fe3a828ad788e0abb35
lemonpick <lemonpick5@gmail.com>
2013-07-27 19:30:21 +0800

    新引导页样式

---
+++ website/application/views/newindex.php
@@ -0,0 +1,16 @@
+<div class="main">
+	<div class="search_bar">
+		<input type="text" class="search_input" placeholder="请输入要查找的书目" />
+		<input type="submit" class="search_submit" value=""/>
+	</div>
+
+	<div class="index">
+		<ul>
+			<li><a href="" class="part_1"><span></span><p>我的书架</p></a></li>
+			<li><a href="" class="part_2"><span></span><p>我要借书</p></a></li>
+			<li><a href="" class="part_3"><span></span><p>我要捐书</p></a></li>
+			<li><a href="" class="part_4"><span></span><p>F&A</p></a></li>
+		</ul>
+	</div>
+	<div class="shelf_bg"><a href=""></a></div>
+</div>
