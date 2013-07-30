5e06fac0b7daa676ac99deb590a8491711b47965
zhangchile <zzlchile@gmail.com>
2013-07-30 13:41:24 +0800

    修复搜素栏

--- website/application/views/guide.php
+++ website/application/views/guide.php
@@ -1,15 +1,13 @@
 <div class="main">
-	<div class="search_bar">
-		<input type="text" class="search_input" placeholder="请输入要查找的书目" />
-		<input type="submit" class="search_submit" value=""/>
-	</div>
+
+<?php include "template/search_bar.php"; ?>
 
 	<div class="index">
 		<ul>
 			<li><a href="<?php echo site_url('my_book');?>" class="part_1"><span></span><p>我的书架</p></a></li>
 			<li><a href="<?php echo site_url('home');?>" class="part_2"><span></span><p>我要借书</p></a></li>
 			<li><a href="<?php echo site_url('add_book');?>" class="part_3"><span></span><p>我要捐书</p></a></li>
-			<li><a href="<?php echo site_url('qa');?>" class="part_4"><span></span><p>F&A</p></a></li>
+			<li><a href="<?php echo site_url('qa');?>" class="part_4"><span></span><p>Q&A</p></a></li>
 		</ul>
 	</div>
 	<div class="shelf_bg"><a href="<?php echo site_url('about');?>"></a></div>
