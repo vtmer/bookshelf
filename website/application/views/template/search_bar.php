3c23209ca16457c17a049cb0a478fec359a10bf6
lemonpick <lemonpick5@gmail.com>
2013-08-03 05:15:55 +0800

    搜索栏样式

--- website/application/views/template/search_bar.php
+++ website/application/views/template/search_bar.php
@@ -1,6 +1,6 @@
 <div class="search_bar">
 	<form action="<?php echo site_url('search'); ?>" method="get"> 
-	<input type="text" class="search_input" placeholder="请输入要查找的书目" name="keywords"/>
+	<input type="text" class="search_input" placeholder=" 请输入要查找的书目" name="keywords"/>
 	<input type="submit" class="search_submit" value=""/>
 	</form>
 </div>
