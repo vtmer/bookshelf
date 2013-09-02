9c0d7514a9c4ac71c69d5d1229e6c240f87e8d8f
zhangchile <zzlchile@gmail.com>
2013-09-02 20:30:54 +0800

    增加saelogo。

--- website/application/views/template/footer.php
+++ website/application/views/template/footer.php
@@ -3,7 +3,7 @@
 	<ul>
 		<li>Copyright © 2013 Product Of <a href="http://weibo.com/vtmer">维生数工作室</a></li>
 		<li><a href="mailto:gdutbookshelf@163.com" title="gdutbookshelf@163.com">联系我们</a> gdutbookshelf@163.com <a href="http://www.vtmer.com">关于我们</a> 工大书架</li>
-
+		<li><div style="scrollbar-arrow-color:buttontext"><img alt="" height="12" src="http://static.sae.sina.com.cn/image/poweredby/117X12px.gif" width="117" ></div></li>	
 	</ul>
 </div>
 </body>
