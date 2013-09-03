24b8a58c65bb4599e9aaa1fd8a1493ce9ad3ec0b
zhangchile <zzlchile@gmail.com>
2013-09-03 11:31:25 +0800

    修复验证码

--- website/application/controllers/captcha.php
+++ website/application/controllers/captcha.php
@@ -8,9 +8,11 @@ class Captcha extends CI_Controller
 	}
 	public function index()
 	{
+		ob_start();
 	  $this->load->library('captcha_code');
 	  $captcha_code = $this->captcha_code->show();
 	  $this->session->set_userdata(array('captcha_code'=>$captcha_code));//将验证码存入session
+	  ob_end_flush();
 	}
 }
 /*----End of captcha.php----*/
